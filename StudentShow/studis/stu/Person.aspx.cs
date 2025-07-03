674bb4858d7e73484f47afc88446918ba57f216a
Jingjing17 <150319823+Jingjing17@users.noreply.github.com>
2025-07-03 12:12:11 +0800

    Add files via upload

---
+++ StudentShow/studis/stu/Person.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class stu_Person : System.Web.UI.Page
+{
+    public SDM.BLL.StudentInfo bll = new SDM.BLL.StudentInfo();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // 获取 session 中保存的 userId（学生学号是 string 类型）
+            string userId = Session["userId"]?.ToString();
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                // 获取数据
+                DataSet ds = bll.GetStudentInfo(userId);
+
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    DataRow row = ds.Tables[0].Rows[0];
+                    lblName.Text = row["UserName"].ToString();
+                    lblSex.Text = row["UserSex"].ToString();
+                    lblNumber.Text = row["UserNumber"].ToString();
+                    lblXy.Text = row["UserXy"].ToString();
+                    lblZy.Text = row["UserZy"].ToString();
+                    lblBj.Text = row["UserBj"].ToString();
+                }
+            }
+        }
+    }
+
+
+}
